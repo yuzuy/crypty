09eeaed676fc6c4bf589042606b7a735970775bf
yuz <re.yuz77777@gmail.com>
2020-02-19 11:21:52 +0900

    Fix typo and add comments

--- crypty.go
+++ crypty.go
@@ -8,15 +8,18 @@ import (
 	"time"
 )
 
-const MaxRandomNumberRange = 234
+// maxRandomNumberRange(=234) is limit of the random number to use crypty
+// If a random number is greater than 234, happen bugs with a limit of unicode
+const maxRandomNumberRange = 234
 
+// Encrypt generates crypty crypto
 func Encrypt(text string) string {
 	var encryptedText string
-	randNumRange := MaxRandomNumberRange
+	randNumRange := maxRandomNumberRange
 	unicodeCodePoints := []rune(text)
 	rand.Seed(time.Now().UnixNano())
 	for _, code := range unicodeCodePoints {
-		if MaxRandomNumberRange*MaxRandomNumberRange-randNumRange*randNumRange < int(code) {
+		if maxRandomNumberRange*maxRandomNumberRange-randNumRange*randNumRange < int(code) {
 			randNumRange -= int(math.Sqrt(float64(code)))
 		}
 	}
@@ -30,17 +33,18 @@ func Encrypt(text string) string {
 	return encryptedText
 }
 
+// Decrypt decrypts text was encrypted with crypty
 func Decrypt(encryptedText string) string {
-	var planeText string
+	var plainText string
 	words := strings.Split(encryptedText, "")
 	for {
 		randNum := math.Sqrt(float64([]rune(words[1])[0]))
 		decryptedWord := string([]rune(words[0])[0] - rune(randNum))
-		planeText += decryptedWord
+		plainText += decryptedWord
 		words = words[2:]
 		if len(words) == 0 {
 			break
 		}
 	}
-	return planeText
+	return plainText
 }

049d71b4fa5c600b20503d802201904e8f025bb1
yuz <re.yuz77777@gmail.com>
2020-02-19 14:14:08 +0900

    Can't use on URL

--- crypty.go
+++ crypty.go
@@ -1,7 +1,6 @@
 package crypty
 
 import (
-	"encoding/base64"
 	"fmt"
 	"math"
 	"math/rand"
@@ -17,31 +16,27 @@ const maxRandomNumberRange = 234
 func Encrypt(text string) string {
 	var encryptedText string
 	randNumRange := maxRandomNumberRange
-	unicodeCodePoints := []rune(text)
+	runes := []rune(text)
 	rand.Seed(time.Now().UnixNano())
-	for _, code := range unicodeCodePoints {
+	for _, code := range runes {
 		if maxRandomNumberRange*maxRandomNumberRange-randNumRange*randNumRange < int(code) {
 			randNumRange -= int(math.Sqrt(float64(code)))
 		}
 	}
-	for _, code := range unicodeCodePoints {
+	for _, code := range runes {
 		randNum := rand.Intn(randNumRange)
 		if randNum == 0 {
 			randNum = 1
 		}
 		encryptedText += fmt.Sprintf("%s", string(code+rune(randNum))+string(rune(randNum*randNum)))
 	}
-	return base64.URLEncoding.EncodeToString([]byte(encryptedText))
+	return encryptedText
 }
 
 // Decrypt decrypts text was encrypted with crypty
-func Decrypt(encryptedTextEncoded string) (string, error) {
+func Decrypt(encryptedText string) (string, error) {
 	var plainText string
-	encryptedTextByte, err := base64.URLEncoding.DecodeString(encryptedTextEncoded)
-	if err != nil {
-		return "", err
-	}
-	words := strings.Split(string(encryptedTextByte), "")
+	words := strings.Split(encryptedText, "")
 	for {
 		randNum := math.Sqrt(float64([]rune(words[1])[0]))
 		decryptedWord := string([]rune(words[0])[0] - rune(randNum))
